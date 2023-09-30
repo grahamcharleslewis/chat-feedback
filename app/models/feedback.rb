39ffbd668839a74df0854f30bfa07b013f9aa606
Graham Charles Lewis <grahamcharleslewis@gmail.com>
2023-09-30 09:08:59 +0100

    Load questions from YAML based on ENV VAR version

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,29 @@
 class Feedback < ApplicationRecord
   belongs_to :chat, optional: true
+
+  def self.message_questions
+    @@message_questions ||= load_questions("message")
+  end
+
+  def message_questions
+    self.class.message_questions
+  end
+
+  def self.conversation_questions
+    @@conversation_questions ||= load_questions("conversation")
+  end
+
+  def conversation_questions
+    self.class.conversation_questions
+  end
+
+private
+
+  def self.load_questions(type)
+    puts "Loading [#{type}] questions..."
+    config = YAML.load_file("data/#{type}.yaml")
+    config[ENV["#{type.upcase}_FEEDBACK_VERSION"]]
+  end
 end
+
+
